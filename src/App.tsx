bba7243ea6aab94fbd0a95087fffc44e192e8061
Luis Bolivar <luisbolivar515@gmail.com>
2022-08-01 23:13:59 -0400

    Adding event component styles

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,8 @@ const mockupEvents = [
     name: "Vacaciones",
     dateFrom: "2022-07-21T12:00",
     dateTo: "2022-07-22T08:45",
-    description: "Example text",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
     type: "Holiday",
   },
   {
--- src/components/calendar/event/Event.tsx
+++ src/components/calendar/event/Event.tsx
@@ -1,6 +1,20 @@
 import { useContext } from "react";
 import CalendarCtx from "../context/calendarContext";
-import Modal from "../UI/Modal";
+import Dialog from "@mui/material/Dialog";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import DialogTitle from "@mui/material/DialogTitle";
+import Typography from "@mui/material/Typography";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import SquareIcon from "@mui/icons-material/Square";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import IconButton from "@mui/material/IconButton";
+import CloseIcon from "@mui/icons-material/Close";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
+import NotesIcon from "@mui/icons-material/Notes";
+
+import CalendarMonthIcon from "@mui/icons-material/CalendarMonth";
 
 const Event = () => {
   const { handleEventDisplayed, handleModalOpenClose, eventDisplayed } =
@@ -8,21 +22,70 @@ const Event = () => {
   if (!eventDisplayed) {
     return <p>No hay evento disponible</p>;
   }
+
   return (
-    <Modal
-      onClose={() => handleModalOpenClose()}
-      title={`${eventDisplayed.name} (${eventDisplayed.type})`}
-      className="eventModal"
+    <Dialog
+      onClose={() => {
+        handleModalOpenClose();
+      }}
+      open={true}
     >
-      <p>
-        From <b>{eventDisplayed.dateFrom}</b> to <b>{eventDisplayed.dateTo}</b>
-      </p>
-      <p>{eventDisplayed.description}</p>
-
-      <button>Change this event</button>
-
-      <button>Delete this event</button>
-    </Modal>
+      <DialogTitle>
+        <IconButton
+          aria-label="settings"
+          sx={{
+            position: "absolute",
+            right: 50,
+            top: 8,
+            color: (theme) => theme.palette.grey[500],
+          }}
+        >
+          <MoreVertIcon />
+        </IconButton>
+        <IconButton
+          aria-label="close"
+          onClick={() => {
+            handleModalOpenClose();
+          }}
+          sx={{
+            position: "absolute",
+            right: 8,
+            top: 8,
+            color: (theme) => theme.palette.grey[500],
+          }}
+        >
+          <CloseIcon />
+        </IconButton>
+      </DialogTitle>
+      <DialogContent>
+        <List>
+          <ListItem>
+            <ListItemIcon>
+              <SquareIcon color="success" />
+            </ListItemIcon>
+            <DialogContentText>
+              <Typography variant="h5" gutterBottom component="div">
+                {eventDisplayed.name}
+              </Typography>
+            </DialogContentText>
+          </ListItem>
+          <ListItem>
+            <ListItemIcon>
+              <NotesIcon />
+            </ListItemIcon>
+            <DialogContentText>{eventDisplayed.description}</DialogContentText>
+          </ListItem>
+          <ListItem>
+            <ListItemIcon>
+              <CalendarMonthIcon />
+            </ListItemIcon>
+            <DialogContentText>
+              From: {eventDisplayed.dateFrom} To: {eventDisplayed.dateTo}
+            </DialogContentText>
+          </ListItem>
+        </List>
+      </DialogContent>
+    </Dialog>
   );
 };
 
